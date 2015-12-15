5f4d00259855b1fab4b511839fa793ac6fbb7cd7
Michael Porras <michael.porras@rackspace.com>
2015-12-15 15:29:41 -0600

    added calling

--- remote/mod_ir/controllers.py
+++ remote/mod_ir/controllers.py
@@ -1,13 +1,21 @@
+import shlex
+
 from flask import Blueprint, request, Response, \
                   jsonify, g, session
 
+from subprocess32 import call
+
 mod_ir = Blueprint('ir', __name__, url_prefix='/ir')
+
+def run_command(device, command):
+    arg_str = 'irsend SEND_ONCE %s %s' % (device, command)
+    args = shlex.split(arg_str)
+    call(args)
+
 
 @mod_ir.route('/send', methods=['PUT'])
 def send_command():
     params = request.args
     target = str(params.get('device'))
     command = str(params.get('command'))
-    print target
-    print command
     return "lol okay!"
